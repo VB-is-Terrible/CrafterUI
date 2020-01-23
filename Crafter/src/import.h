982cd4ecd90761b58f0f78eed3753b6913aa7abf
William Chow <431741580derp@gmail.com>
2020-01-24 08:27:56 +1100

    spacing

--- Crafter/src/import.h
+++ Crafter/src/import.h
@@ -31,5 +31,5 @@ namespace crafter {
 	void read_in(std::string file_name, recipe_store& store);
 
 	std::vector<Ingredients> get_requests_from_file(const crafter::recipe_store& recipes, const std::string& input_file);
-    std::string hello(void);
+	std::string hello(void);
 }
